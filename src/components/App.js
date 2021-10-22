1a2a001d0ee06a9cf971e2e7616151ff0d1dea02
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-22 11:19:39 +0300

    Add Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputComponent from './input';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InputComponent />
+    </div>
+  );
+}
+
+export default App;
