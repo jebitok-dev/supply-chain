c5af80567988716edd7e42d7cefc8d81acad6aa8
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-23 13:48:29 +0300

    Add Changes to Form

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,16 +8,16 @@ import axios from 'axios';
 /* eslint-disable */
 
 const InputForm = () => {
-  const [name, setName] = useState('');
-  const [salary, setSalary] = useState('');
-  const [hobby, setHobby] = useState('');
-  const [age, setAge] = useState('');
+  const [BatchName, setBatchName] = useState('');
+  const [BatchSerialNo, setBatchSerialNo] = useState('');
+  const [Products, setProducts] = useState('');
+  const [Events, setEvents] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const data = {
-      name, salary, hobby, age,
+      BatchName, BatchSerialNo, Products, Events,
     };
     axios.post('https://sheet.best/api/sheets/e972f109-e16d-44b7-8177-a1fe9345f648', data)
       .then((response) => {
@@ -33,28 +33,28 @@ const InputForm = () => {
 					<label>Batch Name</label>
 					<input
 						placeholder="Enter the Batch Name"
-						onChange={(e) => setName(e.target.value)}
+						onChange={(e) => setBatchName(e.target.value)}
 					/>
 				</Form.Field>
 				<Form.Field>
 					<label>Batch Serial No.</label>
 					<input
 						placeholder="Enter the Batch Serial No."
-						onChange={(e) => setAge(e.target.value)}
+						onChange={(e) => setBatchSerialNo(e.target.value)}
 					/>
 				</Form.Field>
 				<Form.Field>
 					<label>Product Details</label>
 					<input
 						placeholder="Enter the Product Details"
-						onChange={(e) => setSalary(e.target.value)}
+						onChange={(e) => setProducts(e.target.value)}
 					/>
 				</Form.Field>
 				<Form.Field>
 					<label>No. of Events</label>
 					<input
 						placeholder="Enter the No. of Events"
-						onChange={(e) => setHobby(e.target.value)}
+						onChange={(e) => setEvents(e.target.value)}
 					/>
 				</Form.Field>
 
