4f6d4c528917afb336a11d7a21a3fca8571ef29b
jebitok-dev <sharonkosgei4@gmail.com>
2021-10-23 17:24:48 +0300

    Add Changes to Sections

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,8 @@ const About = ({ isClicked }) => (
     <h2 className="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.1s">What Is SmartTrace</h2>
     <div className="about-wrapper d-flex flex-column flex-md-row wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.1s">
       <div>
-        <p>SmartTrace is a digital tracer and verifier that allows supply chain actors to record data of the varoius value chain batch endpoints</p>
-        <p>Data is stored on the blockchain for security and storage. With this data the user can generate QR Code that can be printed out into document</p>
-        <p>and stored on blockchain in hash rate format. The QR Code can be attached on end product for end-user or buyer to check various VC information.</p>
+        <p>SmartTrace is a digital tracer and verifier that allows supply chain actors to record data of the varoius svalue chain endpoints</p>
+        <p>The success of the SmartOne will be totally achieved with a global community of supply chain actors of various Value Chains and researchers with blockchain experts who will create tools and research data, to help make this successful especially with complaince</p>
       </div>
       <div className="w-100 about-img-container d-flex justify-center"><img className="about-img" src="images/about.png" alt="" /></div>
     </div>
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
+
+/* eslint-disable*/ 
+
+const Footer = ({ twitter, linkedin, github }) => (
+  <footer>
+    <div className="footer-wrapper py-5">
+      <ul className="list-unstyled">
+        <li className="my-auto d-flex">
+          <NavLink exact to="/"><img src="images/logo.png" alt="company logo" className="logo mr-auto " /></NavLink>
+        </li>
+      </ul>
+      <ul className="list-unstyled footer-nav d-md-flex pb-3">
+        <li className="my-auto mr-3 py-1"><NavLink to="/about">About</NavLink></li>
+        <li className="my-auto py-1"><Link to="/#team">Contact</Link></li>
+        <li className="my-auto ml-auto py-1"><button className="lightmodeButton"><Link to="/useApp">Use SmartOne</Link></button></li>
+      </ul>
+      <div id="hr" />
+      <div className="d-flex py-3">
+        <p className="my-auto">&copy; 2021 SmartOne</p>
+        <ul className="socials d-flex list-unstyled ml-auto my-auto">
+          <li className="mx-1"><a href="https://github.com/jebitok-dev/supplychain" target="_hello"><img src={github} alt="github" width="20px" /></a></li>
+          <li className="mx-1"><a href="kk" target="_hi"><img src={linkedin} alt="linkedin" width="20px" /></a></li>
+          <li className="mx-1"><a href="kk" target="_hey"><img src={twitter} alt="twitter" width="20px" /></a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+);
+
+export default withRouter(Footer);
